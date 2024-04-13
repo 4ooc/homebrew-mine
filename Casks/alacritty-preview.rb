d5b7e619c69e5e97831682cd07aa71955d93481a
4ooc <22727824+4ooc@users.noreply.github.com>
2024-04-13 23:45:18 +0800

    upgrade(alacritty-preview): 0.13.2

--- Casks/alacritty-preview.rb
+++ Casks/alacritty-preview.rb
@@ -1,6 +1,6 @@
 cask "alacritty-preview" do
-  version "0.13.0-rc2"
-  sha256 "267e2f0f7da15b718da2e95077524794934c57a3c12c981bde6c15284796c0ab"
+  version "0.13.2"
+  sha256 "c71ce23fc365c4d046de2a48161d26232a5734c519e6b5ff3f55c60258260f60"
 
   url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
   name "Alacritty"
